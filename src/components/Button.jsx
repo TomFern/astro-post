3865ac1f35a7b7654a74df398d3ae26316e5e4da
tomas f <46322567+TomFern@users.noreply.github.com>
2023-01-17 15:53:30 -0300

    update

---
+++ src/components/Button.jsx
@@ -0,0 +1,10 @@
+export default function MyButton() {
+
+const clicked = () => { console.log('Hello!') };
+
+return (
+    <div>
+      <button style={{ color: 'purple' }} onClick={clicked}>Click me</button>
+    </div>
+  )
+}
